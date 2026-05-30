a9c1fbccf35576dbf700155dabe13e0e3d21b5cf
Aditi Shreya <chanchu.archanamoon@gmail.com>
2026-05-31 03:25:10 +0530

    Add files via upload

---
+++ Prime_numbers_.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int n;
+printf("Enter any number");
+scanf("%d",&n);
+int a=0;
+for(int i=2;i<=n-1;i++){
+    if(n%i==0){
+        a=1;
+        break;
+ }
+}
+if(a==0) printf("Given number is prime");
+else printf("Given number is composite");
+return 0;
+}
