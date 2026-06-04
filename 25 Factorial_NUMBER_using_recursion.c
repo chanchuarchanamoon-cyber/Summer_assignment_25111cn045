b1103927c141af69289f80a01752f7d5d576991f
Aditi Shreya <chanchu.archanamoon@gmail.com>
2026-06-05 01:56:03 +0530

    Add files via upload

---
+++ 25 Factorial_NUMBER_using_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int fact(int x){
+    int i;
+    int fact=fact*i;
+}
+int main(){
+    int a;
+    printf("Enter value of a number");
+scanf("%d",&a);
+printf("Factorial of number is: %d",fact);
+return 0;
+}
