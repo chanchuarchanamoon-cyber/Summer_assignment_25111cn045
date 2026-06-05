86615129ce06b05efedb9b15b002598d2194e6cb
Aditi Shreya <chanchu.archanamoon@gmail.com>
2026-06-06 02:41:39 +0530

    Add files via upload

---
+++ Day 6/Q21.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+int binaryToDecimal(long long n){
+    int i,int n;
+while(n!=0){
+    int remainder=n%10;
+    n=n/10;
+   int decimalNumber=decimalNumber+remainder*pow(2,i)
+    n++
+}
+}
+int main(){
+    int decimalNumber;
+    int n;
+    printf("Enter a number");
+    scanf("lld",&n);
+    printf("Number is",decimalNumber);
+    return 0;
+}
