3d5a5285cc526ae677295c251dcaeedec16408d6
Aditi Shreya <chanchu.archanamoon@gmail.com>
2026-06-10 20:38:08 +0530

    Add files via upload

---
+++ Day 8/Q29.c
@@ -0,0 +1,14 @@
+
+int main() {
+    int n = 5;
+
+    // Outer loop for printing rows
+    for (int i = 0; i < n; i++) {
+
+        // Inner loop for printing * in each rows
+        for (int j = 0; j <= i; j++)
+            printf("* ");
+        printf("\n");
+    }
+    return 0;
+}
